abc28f87192aff509ace2e2a372672344e2983b9
AlexLaBree <75798879+AlexLaBree@users.noreply.github.com>
2021-05-20 12:57:14 -0400

    added wall, pizza home scorepanel and robot objects

--- RobotWorld.java
+++ RobotWorld.java
@@ -17,5 +17,20 @@ public class RobotWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        addObject (new Robot(), 20, 40);
+        addObject (new Wall(), 50, 150);
+        addObject (new Wall(), 155, 150);
+        addObject (new Wall(), 260, 150);
+        addObject (new Wall(), 750, 150);
+        addObject (new Wall(), 645, 150);
+        addObject (new Wall(), 540, 150);
+        addObject (new Block(), 400, 150);
+        addObject (new Home(), 750, 550);
+        addObject (new ScorPanel(), 100, 530);
+        addObject (new Pizza(), 750, 50);
+        addObject (new Pizza(), 425, 50);
+        addObject (new Pizza(), 650, 360);
+        addObject (new Pizza(), 425, 500);
+        addObject (new Pizza(), 250, 300);
     }
 }
--- Wall.java
+++ Wall.java
@@ -15,5 +15,6 @@ public class Wall extends Actor
     public void act() 
     {
         // Add your action code here.
+        setLocation(40,40);
     }    
 }
